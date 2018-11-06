b4ad349c1f05ae6c51f581362a9d8ba148eb34c7
klauritz <klauritz@hawaii.edu>
2018-11-05 21:18:28 -1000

    polished infoActivity UI and added theming based on conservation status

--- android/tflite/app/src/main/java/com/example/android/flowerai/InfoActivity.java
+++ android/tflite/app/src/main/java/com/example/android/flowerai/InfoActivity.java
@@ -1,12 +1,11 @@
 package com.example.android.flowerai;
 
 import android.app.Activity;
-import android.net.Uri;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.net.URI;
 
 /**
  * {@code Activity} class for Search Info.
@@ -29,34 +28,70 @@ public class InfoActivity extends Activity {
         TextView familyTV = findViewById(R.id.familyTextView);
         TextView nativeTV = findViewById(R.id.nativeTextView);
         ImageView plantIV = findViewById(R.id.flowerImage);
+        ImageView imageCrestIV = findViewById(R.id.imageCrest);
 
-        switch(plant.common_name) {
-            case "Plumeria":
+        Button reportButton = findViewById(R.id.reportSIghtingBtn);
+
+        switch(plant.common_name.toUpperCase()) {
+            case "PLUMERIA":
                 plantIV.setImageResource(R.drawable.plumeria);
                 break;
-            case "Bird of Paradise":
+            case "BIRD OF PARADISE":
                 plantIV.setImageResource(R.drawable.birdofparadise);
                 break;
-            case "Ohia":
+            case "OHIA":
                 plantIV.setImageResource(R.drawable.ohia);
                 break;
-            case "Hibiscus":
+            case "HIBISCUS":
                 plantIV.setImageResource(R.drawable.hibiscus);
+                break;
+            case "EKE SILVERSWORD":
+                plantIV.setImageResource(R.drawable.ekesilversword);
+                break;
+            case "KO‘OKO‘OLAU":
+                plantIV.setImageResource(R.drawable.bidens_wiebkei);
                 break;
             default:
                 plantIV.setImageResource(R.drawable.noimage);
                 break;
         }
 
-//        plantIV.setImageURI(R.drawable.birdofparadise);
-
 
         // Strings from Plant Class
-        commonNameTV.setText(plant.common_name);
-        sciNameTV.setText(plant.name);
-        statusTV.setText(plant.conservation_status);
-        familyTV.setText(plant.family);
-        nativeTV.setText(plant.native_status);
+        commonNameTV.setText(plant.common_name.toUpperCase());
+        sciNameTV.setText(plant.name.toUpperCase());
+        statusTV.setText(plant.conservation_status.toUpperCase());
+        familyTV.setText(plant.family.toUpperCase());
+        nativeTV.setText(plant.native_status.toUpperCase());
+
+        switch(plant.conservation_status.toUpperCase()) {
+            case "LEAST CONCERN":
+                sciNameTV.setTextColor(getResources().getColor(R.color.green_300));
+                statusTV.setTextColor(getResources().getColor(R.color.green_300));
+                reportButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.green_800)));
+                imageCrestIV.setImageResource(R.drawable.circle_green);
+                break;
+            case "ENDANGERED":
+                sciNameTV.setTextColor(getResources().getColor(R.color.yellow_300));
+                statusTV.setTextColor(getResources().getColor(R.color.yellow_300));
+                reportButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.yellow_800)));
+                imageCrestIV.setImageResource(R.drawable.circle_yellow);
+                break;
+            case "RARE":
+                sciNameTV.setTextColor(getResources().getColor(R.color.red_300));
+                statusTV.setTextColor(getResources().getColor(R.color.red_300));
+                reportButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.red_800)));
+                imageCrestIV.setImageResource(R.drawable.circle_red);
+                break;
+            default:
+                sciNameTV.setTextColor(getResources().getColor(R.color.green_300));
+                statusTV.setTextColor(getResources().getColor(R.color.green_300));
+                reportButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.green_800)));
+                imageCrestIV.setImageResource(R.drawable.circle_green);
+                break;
+
+
+        }
 
     }
 }
