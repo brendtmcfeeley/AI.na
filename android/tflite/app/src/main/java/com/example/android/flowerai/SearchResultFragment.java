3c17dbeaec1f87299e7777307300d2c6db718ee8
Brendt McFeeley <brendtmcfeeley@gmail.com>
2018-11-05 22:45:07 -1000

    updated fragment loading for dynamic database data

--- android/tflite/app/src/main/java/com/example/android/flowerai/SearchResultFragment.java
+++ android/tflite/app/src/main/java/com/example/android/flowerai/SearchResultFragment.java
@@ -30,62 +30,109 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 
-/** Basic fragments for the Camera. */
+/**
+ * Basic fragments for the Camera.
+ */
 public class SearchResultFragment extends Fragment {
 
-  @Override
-  public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    FirebaseDatabase mFirebaseDatabase;
-    DatabaseReference myRef;
-    mFirebaseDatabase = FirebaseDatabase.getInstance();
-    myRef = mFirebaseDatabase.getReference();
+        FirebaseDatabase myFirebaseDatabase;
+        DatabaseReference myRef;
+        myFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = myFirebaseDatabase.getReference();
 
-    ArrayList<String> nameList = new ArrayList<>();
+        ArrayList<String> nameList = new ArrayList<>();
 
-    myRef.addValueEventListener(new ValueEventListener() {
-      @Override
-      public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-        for (DataSnapshot ds : dataSnapshot.getChildren()) {
-          String nameValue = String.valueOf(ds.getKey());
+                ArrayList<String> fragArgs = new ArrayList<>();
 
-          nameList.add(nameValue);
+                try {
+                    fragArgs = getArguments().getStringArrayList("databaseArgs");
+                } catch (NullPointerException npe) {
+                }
 
-        }
+                if (fragArgs.get(1).equals("focusChange")) {
+                    nameList.clear();
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                        String nameValue = String.valueOf(ds.getKey());
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                container.getContext(),
-                android.R.layout.simple_selectable_list_item,
-                nameList);
+                        nameList.add(nameValue);
+                    }
+                }
+                else if (fragArgs.get(1).equals("textSubmit")){
+                    String nameValue;
 
-        ListView listView = getView().findViewById(R.id.listview);
-        listView.setAdapter(adapter);
+                    if (dataSnapshot.child(fragArgs.get(0)).getValue() == null) {
+                        nameValue = "Sorry, could not find the plant : " + fragArgs.get(0);
+                        nameList.clear();
+                        nameList.add(nameValue);
+                    } else {
+                        nameList.clear();
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            nameValue = String.valueOf(ds.getKey());
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //here u can use clickListener
-          @Override
-          public void onItemClick(final AdapterView<?> parent, View view, final int position, long id) {
-            view.setSelected(true);
-          }
+                            if (nameValue.toLowerCase().contains(fragArgs.get(0).toLowerCase())) {
+                                nameList.add(nameValue);
+                            }
+                        }
+                    }
+                }
+                else {
+                    String nameValue;
+                    nameList.clear();
+
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                        nameValue = String.valueOf(ds.getKey());
+
+                        if (nameValue.toLowerCase().contains(fragArgs.get(0).toLowerCase())) {
+                            nameList.add(nameValue);
+                        }
+                    }
+
+                    if (nameList.size() <= 0) {
+                        nameValue = "Sorry, could not find the plant : " + fragArgs.get(0);
+                        nameList.add(nameValue);
+                    }
+                }
+
+
+                final ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                        container.getContext(),
+                        android.R.layout.simple_selectable_list_item,
+                        nameList);
+
+                ListView listView = getView().findViewById(R.id.listview);
+                listView.setAdapter(adapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //here u can use clickListener
+                    @Override
+                    public void onItemClick(final AdapterView<?> parent, View view, final int position, long id) {
+                        view.setSelected(true);
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
         });
-      }
-
-      @Override
-      public void onCancelled(@NonNull DatabaseError databaseError) {
-
-      }
-    });
 
 
-
-    return inflater.inflate(R.layout.fragment_search_result, container, false);
-  }
+        return inflater.inflate(R.layout.fragment_search_result, container, false);
+    }
 
 
 }
